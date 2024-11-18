b0958814bb50ec136e2bba5af9fd3d335993fa1d
João Pereira <johnnypereiradacostajr@gmail.com>
2024-11-18 20:03:52 -0300

    projetoReact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import EventoAlert from './componentes/EventoAlert';
+import MudaCorMouse from './componentes/MudaCorMouse';
+import AlterButton from './componentes/AlterButton';
+import ListaDeFrutas from './componentes/ListaDeFrutas';
+import Fragmento from './componentes/Fragmento';
 
 function App() {
   return (
-    <div>
-      <EventoAlert/>
-    </div>
+    <>
+      <Fragmento/>
+    </>
   );
 }
 
